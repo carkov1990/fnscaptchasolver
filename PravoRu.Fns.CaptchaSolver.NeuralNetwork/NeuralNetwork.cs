7d81008b56770d41470a7df6fa5bc58844d4b2c5
Sergey Tsarkov <carkov1990@gmail.com>
2019-07-06 09:58:38 +0400

    net462-

--- PravoRu.Fns.CaptchaSolver.NeuralNetwork/NeuralNetwork.cs
+++ PravoRu.Fns.CaptchaSolver.NeuralNetwork/NeuralNetwork.cs
@@ -1,7 +1,19 @@
+using System.Collections.Generic;
+
 namespace PravoRu.Fns.CaptchaSolver.NeuralNetwork
 {
 	public class NeuralNetwork
 	{
-		
+		private readonly List<Layer> _layers;
+
+		public NeuralNetwork(int layerCount,params int[] neuronLayerCount)
+		{
+			_layers = new List<Layer>(layerCount);
+			foreach (var layer in _layers)
+			{
+				
+			}
+		}
+
 	}
 }
